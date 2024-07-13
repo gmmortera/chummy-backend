c1455b516d768f4173971980b6837350e86898f3
gianfranco <gfmmortera@gmail.com>
2024-07-13 20:57:57 +0800

    Add a custom action

--- backend/app/utils/CHError.scala
+++ backend/app/utils/CHError.scala
@@ -2,17 +2,17 @@ package utils
 
 import play.api.mvc.Results
 import play.api.http.Status
-import play.api.i18n.Messages
 
 case class ServiceError(
   status: Int,
   code: String,
   cause: Throwable = null
 ) extends Exception(code, cause) {
-  def toResult(implicit messages: Messages) = Results.Status(status)(messages(code))
+  def toResult = Results.Status(status)(code)
 }
 
 trait CHError {
   val USER_ERROR_REGISTER = new ServiceError(Status.FORBIDDEN, "user.error.register")
   val USER_ERROR_LOGIN = new ServiceError(Status.UNAUTHORIZED, "user.error.login")
+  val AUTH_ERROR = new ServiceError(Status.UNAUTHORIZED, "auth.error")
 }
