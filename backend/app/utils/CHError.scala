dcb413482bc87e54c288fa26e765e77d68dc9c7f
gianfranco <gfmmortera@gmail.com>
2024-07-24 22:47:42 +0800

    Reformed custom error

--- backend/app/utils/CHError.scala
+++ backend/app/utils/CHError.scala
@@ -3,16 +3,14 @@ package utils
 import play.api.mvc.Results
 import play.api.http.Status
 
-case class ServiceError(
+case class CHError(
   status: Int,
   code: String,
   cause: Throwable = null
-) extends Exception(code, cause) {
-  def toResult = Results.Status(status)(code)
-}
+) extends Exception(code, cause)
 
-trait CHError {
-  val USER_ERROR_REGISTER = new ServiceError(Status.FORBIDDEN, "user.error.register")
-  val USER_ERROR_LOGIN = new ServiceError(Status.UNAUTHORIZED, "user.error.login")
-  val AUTH_ERROR = new ServiceError(Status.UNAUTHORIZED, "auth.error")
+object CHErrorHandler {
+  def apply(error: CHError) = {
+    Results.Status(error.status)(error.code)
+  }
 }
