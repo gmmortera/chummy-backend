59ba2e56cf36ba0faa5ca1f784a90d5ad64cfbea
gianfranco <gfmmortera@gmail.com>
2024-08-07 23:27:04 +0800

    Add a reply domain

---
+++ backend/app/models/domain/Reply.scala
@@ -0,0 +1,38 @@
+package models.domain
+
+import java.util.UUID
+import java.time.Instant
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+
+case class Reply(
+  id: UUID,
+  idUser: UUID,
+  idComment: UUID,
+  text: String,
+  createdAt: Instant,
+  updatedAt: Option[Instant]
+) {
+  def withId(id: UUID): Reply = new Reply(
+    id,
+    idUser,
+    idComment,
+    text,
+    createdAt,
+    Some(Instant.now)
+  )
+}
+
+object Reply {
+  implicit val replyReads: Reads[Reply] = (
+    Reads.pure(UUID.randomUUID) and
+    (JsPath \ "idUser").read[UUID] and
+    (JsPath \ "idComment").read[UUID] and
+    (JsPath \ "text").read[String] and
+    Reads.pure(Instant.now) and
+    (JsPath \ "updatedAt").readNullable[Instant]
+  )(Reply.apply _)
+  implicit val replyWrites: Writes[Reply] = Json.writes[Reply]
+}
