59a234ae30948cb989dc29574cf29a4326a40066
Gianfranco M. Mortera <gmmortera@vauldex.com>
2024-08-09 14:27:14 +0800

    Add notification controller

---
+++ backend/app/controllers/NotificationController.scala
@@ -0,0 +1,44 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+
+import scala.concurrent.{ Future, ExecutionContext }
+
+import models.domain.Notification
+import models.service.NotificationService
+import utils.{ SecureAction, CHErrorHandler }
+
+@Singleton
+class NotificationController @Inject()(
+  val controllerComponents: ControllerComponents,
+  notifService: NotificationService,
+  SecureAction: SecureAction
+)(implicit ec: ExecutionContext) extends BaseController {
+
+  def index = SecureAction.async {
+    notifService.getNotifications.map(notifications => Ok(Json.obj("notifications" -> Json.toJson       (notifications))))
+  }
+
+  def create = SecureAction.async(parse.json) { request =>
+    val json = request.body.validate[Notification]
+    json.fold(
+      error => Future.successful(BadRequest(Json.obj("error" -> JsError.toJson(error)))),
+      notification => {
+        notifService.createNotification(notification).fold(CHErrorHandler(_), success => Created(Json.obj("success" -> s"$success")))
+      }
+    )
+  }
+
+  def edit = SecureAction.async(parse.json) { request =>
+    val json = request.body.validate[Notification]
+    json.fold(
+      error => Future.successful(BadRequest(Json.obj("error" -> JsError.toJson(error)))),
+      notification => {
+        notifService.editNotification(notification).fold(CHErrorHandler(_), success => Created(Json.obj("success" -> s"$success")))
+      }
+    )
+  }
+}
