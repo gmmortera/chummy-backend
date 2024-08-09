8563a810b6b2794dd08485179fcf84c927ef8bd8
Gianfranco M. Mortera <gmmortera@vauldex.com>
2024-08-09 14:27:06 +0800

    Add notification domain

---
+++ backend/app/models/domain/Notification.scala
@@ -0,0 +1,37 @@
+package models.domain
+
+import java.util.UUID
+import java.time.Instant
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+case class Notification(
+  id: UUID,
+  idUser: UUID,
+  idPost: UUID,
+  action: ActionType.ActionType,
+  createdAt: Instant,
+  seenAt: Option[Instant]
+)
+
+object Notification {
+  implicit val notifReads: Reads[Notification] = (
+    Reads.pure(UUID.randomUUID) and
+    (JsPath \ "idUser").read[UUID] and
+    (JsPath \ "idPost").read[UUID] and
+    (JsPath \ "action").read[String] and
+    Reads.pure(Instant.now) and
+    (JsPath \ "seenAt").readNullable[Instant]
+  )((id, idUser, idPost, action, createdAt, seenAt) => 
+      Notification(id, idUser, idPost, ActionType.withName(action), createdAt, seenAt)
+    )
+  implicit val notifWrites: Writes[Notification] = Json.writes[Notification]
+}
+
+case object ActionType extends Enumeration {
+  type ActionType = Value
+  val REPLY = Value("REPLY")
+  val COMMENT = Value("COMMENT")
+  val LIKE = Value("LIKE")
+}
