0f8f453d63f3922749a99725ac3e47f80af4c184
gianfranco <gfmmortera@gmail.com>
2024-09-27 21:04:33 +0800

    Update general manager actor to include new class and updated params

--- backend/app/actors/GeneralManager.scala
+++ backend/app/actors/GeneralManager.scala
@@ -1,26 +1,28 @@
 package actors
 
+import java.util.UUID
+
 import play.api.libs.json._
 
 import org.apache.pekko.actor.{ Actor, ActorRef, Props }
 
-import models.domain.WebsocketMessage
+import models.domain._
 
 class GeneralManager extends Actor {
   import GeneralManager._
 
-  def receive: Receive = init(Seq.empty[ActorRef])
+  def receive: Receive = init(Seq.empty[ActorData])
 
-  def init(users: Seq[ActorRef]): Receive = {
+  def init(users: Seq[ActorData]): Receive = {
     case NewUser(user) => context.become(init(users :+ user))
-    case NewPost(user, post) => for (u <- users) {
-      if (u != user) u ! UserActor.SendUpdate(post)
+    case NewPost(id, post) => for (u <- users) {
+      if (u.id != id) u.ref ! UserActor.SendUpdate(post)
     }
-    case NewComment(user, comment) => for (u <- users) {
-      if (u != user) u ! UserActor.SendUpdate(comment)
+    case NewComment(id, comment) => for (u <- users) {
+      if (u.id != id) u.ref ! UserActor.SendUpdate(comment)
     }
-    case NewState(user, like) => for (u <- users) {
-      if (u != user) u ! UserActor.SendUpdate(like)
+    case NewLike(id, like) => for (u <- users) {
+      if (u.id != id) u.ref ! UserActor.SendUpdate(like)
     }
   }
 }
@@ -28,8 +30,8 @@ class GeneralManager extends Actor {
 object GeneralManager {
   def props = Props(new GeneralManager)
 
-  case class NewUser(user: ActorRef)
-  case class NewPost(user: ActorRef, post: WebsocketMessage)
-  case class NewComment(user: ActorRef, comment: WebsocketMessage)
-  case class NewState(user: ActorRef, like: WebsocketMessage)
+  case class NewUser(user: ActorData)
+  case class NewPost(id: UUID, post: UserAction)
+  case class NewComment(id: UUID, comment: UserAction)
+  case class NewLike(id: UUID, like: UserAction)
 }
